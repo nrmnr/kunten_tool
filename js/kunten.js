7d144b2c43cf46cb62a1b8c104da48c0e71ec0e8
n-fujita <n-fujita@daiichi-g.co.jp>
2012-09-07 20:32:25 +0900

    add ContextWrapper class

--- js/kunten.js
+++ js/kunten.js
@@ -6,25 +6,53 @@ function(){
 
 	var jtext_src = $('#kunten_src');
 	var jcanvas_kun = $('#kunten');
+	var canvas_kun = jcanvas_kun.get(0);
 
-	var init_canvas_scale = function(){
-		jcanvas_kun.attr('width', parseInt(jcanvas_kun.css('width')));
-		jcanvas_kun.attr('height', parseInt(jcanvas_kun.css('height')));
+	var ContextWrapper = function(canvas){
+		var context = canvas.getContext('2d');
+		var w = canvas.width;
+		var h = canvas.height;
+		var font_size = 48;
+		var font_name = 'ＭＳ 明朝';
+		var pitch = font_size * 1.5;
+
+		context.font = font_size + 'px ' + font_name;
+		context.textAlign = 'center';
+		context.textBaseline = 'top';
+
+		this.get = function(){ return context; };
+
+		this.fillText = function(text,x,y){
+			$.each(
+				text,
+				function(i,t){
+					context.fillText(t,x,y+i*pitch);
+				});
+		};
 	};
 
-	var draw_test = function(context){
+	var init_canvas_scale = function(context){
+		var w = parseInt(jcanvas_kun.css('width'));
+		var h = parseInt(jcanvas_kun.css('height'));
+		jcanvas_kun.attr('width', w);
+		jcanvas_kun.attr('height', h);
+	};
+
+	var draw_test = function(context_wrap){
+		var context = context_wrap.get();
 		context.beginPath();
 		context.moveTo(0,0);
-		context.lineTo(400,600);
-		context.moveTo(400,0);
-		context.lineTo(0,600);
+		context.lineTo(50,100);
+		context.moveTo(50,0);
+		context.lineTo(0,100);
 		context.stroke();
+		context_wrap.fillText('あいうえお',50,0);
 	};
 
 	var draw_kunten = function(){
 		var src = jtext_src.val();
 		init_canvas_scale();
-		var context = jcanvas_kun.get(0).getContext('2d');
+		var context = new ContextWrapper(canvas_kun);
 		draw_test(context);
 	};
 
